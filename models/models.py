f2361cdf15aed13fae18184b78581d43f7eaae52
Jacob Torres <jacob@jacobtorres.net>
2021-02-15 14:55:43 -0700

    Initialized with main project files.

---
+++ models/models.py
@@ -0,0 +1,17 @@
+"""Data models for the application."""
+
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+
+
+class UserTable(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    handle = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class TweetTable(db.Model):
+
+
