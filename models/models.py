d439c1283401d80eaf41eea94b1b633046d231c8
Jacob Torres <jacob@jacobtorres.net>
2021-02-19 21:01:47 -0700

    Updated requirements.txt

--- models/models.py
+++ models/models.py
@@ -8,9 +8,10 @@ db = SQLAlchemy(app)
 class UserTable(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    handle = db.Column(db.String(120), unique=True, nullable=False)
+    handle = db.Column(db.String(80), unique=True, nullable=False)
 
 
 class TweetTable(db.Model):
-
+    id = db.Column(db.BigInteger, primary_key=True)
+    user = db.Column(db.String(80) foreign_key='')
 
